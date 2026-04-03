4cdeff20c8d9e0c6488f1ad44c09e3e89e4a1623
Davi Bernardo <sbernardo.davi@outlook.com>
2026-04-03 15:12:57 -0300

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto p-10">
+      <h1 className="text-4xl font-bold">Meu Blog com React</h1>
+      <p className="mt-4 text-gray-600">Bem-vindo ao meu novo projeto!</p>
+    </main>
+  );
+}
