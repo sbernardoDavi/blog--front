3913edc75b08745df1319d6446771a65771ec9bd
Davi Bernardo <sbernardo.davi@outlook.com>
2026-05-08 19:05:06 -0300

    fix: change logo

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import logoLadp1 from "../../assets/logo-ladp1.png";
+import logo from "../../assets/logo.png";
 import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="logo-center">
       <div className="logo-box">
-        <Image src={logoLadp1} alt="Logo LADP" unoptimized />
+        <Image src={logo} alt="Logo LADP" unoptimized />
       </div>
     </div>
   );
